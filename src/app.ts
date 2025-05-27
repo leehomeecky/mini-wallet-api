9559bee8b1c3c31e1b40a04733255788952100f0
leehomeecky <meecky4christ@gmail.com>
2025-05-27 04:41:29 +0100

    rate limiting

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,20 @@ import authRoutes from './routes/auth.routes';
 import walletRoutes from './routes/wallet.routes';
 import webhookRoutes from './routes/webhook.routes';
 import { MONGO_URI, PORT } from './config/env.config';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
+const limiter = rateLimit({
+  max: 100,
+  legacyHeaders: false,
+  standardHeaders: true,
+  windowMs: 1 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again later.',
+});
+
 app.use(cors());
+app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 
